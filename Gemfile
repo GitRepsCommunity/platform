755be3fb3c11fa875ee0ae370fe5207ea74b3ab6
Andrew J <59379906+andrewjumanca@users.noreply.github.com>
2023-08-16 19:29:48 +0200

    Devise Authentication User Model (#25)

--- Gemfile
+++ Gemfile
@@ -76,4 +76,5 @@ group :test do
   gem 'webdrivers'
 end
 
+gem "devise", "~> 4.9"
 gem "github_api"
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable,
+         :registerable,
+         :rememberable,
+         :validatable
+end
