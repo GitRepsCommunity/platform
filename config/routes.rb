b02aba4b4b5c94ade080bd6dc9a861cd45724a5b
Tim Pile <tim@gitreps.com>
2023-08-17 13:41:28 -0700

    platform 26: UI for user sign up view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
