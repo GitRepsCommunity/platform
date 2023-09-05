e17feba39460f4958744bd223e6fcfcbf8c84de7
Andrew J <jumanca@uw.edu>
2023-09-04 18:25:27 -0700

    Began database changes to prepare for processing of Github API data and user session storage

---
+++ db/migrate/20230905012156_add_omniauth_to_users.rb
@@ -0,0 +1,7 @@
+class AddOmniauthToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :token, :string
+  end
+end
