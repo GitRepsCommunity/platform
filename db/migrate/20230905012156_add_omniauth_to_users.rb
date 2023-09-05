8be6d6a5ac3d5c7d6739a8efd1105e1f1d4269a9
Andrew J <jumanca@uw.edu>
2023-09-05 16:00:41 -0700

    migrated database

--- db/migrate/20230905012156_add_omniauth_to_users.rb
+++ db/migrate/20230905012156_add_omniauth_to_users.rb
@@ -2,6 +2,6 @@ class AddOmniauthToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
-    add_column :users, :token, :string
+    # add_column :users, :token, :string
   end
 end
