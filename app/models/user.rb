fa0c5069612eb48dbef44985516a6cd73ebf516d
Andrew J <jumanca@uw.edu>
2023-09-14 17:35:27 -0700

    Added from_omniauth function to pass through auth information to user schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,16 @@ class User < ApplicationRecord
          :rememberable,
          :validatable,
          :omniauthable, omniauth_providers: %i[github]
+  
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth['provider'], uid: auth['uid']) do |user|
+      primary_email_data = auth['extra']['all_emails'].find { |email_hash| email_hash['primary'] == true }
+      user.email = primary_email_data ? primary_email_data['email'] : auth['info']['email']
+      user.encrypted_password = Devise.friendly_token[0, 20]
+
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 end
