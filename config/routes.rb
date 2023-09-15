c7cd580f2a774f0efb2a3b7f776d6a75177d8855
Andrew J <jumanca@uw.edu>
2023-09-14 18:09:06 -0700

    Added controller programming and authentication from omniauth. Storing user data in user model.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
