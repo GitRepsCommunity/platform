8b67c93f0cc1b85f4323f69a37d5a707fb65005d
Andrew J <jumanca@uw.edu>
2023-09-27 12:07:48 -0700

    Cleaned up vulnerabilities and user_test file, moved json response into fixtures directory, and refactored github function in omniauth callbacks controller.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,37 +1,11 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'json'
 
 class UserTest < ActiveSupport::TestCase
-  # rubocop:disable Metrics/BlockLength
-
   test 'from_omniauth' do
-    raw_auth_response = {
-      'provider' => 'github',
-      'uid' => '59379906',
-      'info' => {
-        'nickname' => 'andrewjumanca',
-        'email' => 'andrewjumanca@gmail.com',
-        'image' => 'https://avatars.githubusercontent.com/u/59379906?v=4'
-      },
-      'credentials' => {
-        'token' => '{\"p\":\"PO/SR+xSiR7NsxNdhwfiG8N8HqAAlXdqCQsJvdOfulxuSypzVAG+YQ==\",' \
-                   '\"h\":{\"iv\":\"VwxVWLF/5FO3Xt1K\",\"at\":\"WahZ6O4/aSdbTZXcgXmjnQ==\"}}'
-      },
-      'extra' => {
-        'raw_info' => {
-          'login' => 'andrewjumanca',
-          'avatar_url' => 'https://avatars.githubusercontent.com/u/59379906?v=4',
-          'url' => 'https://api.github.com/users/andrewjumanca',
-          'html_url' => 'https://github.com/andrewjumanca'
-        },
-        'all_emails' => [
-          { 'email' => '59379906+andrewjumanca@users.noreply.github.com' },
-          { 'email' => 'jumanca@uw.edu' },
-          { 'email' => 'andrewjumanca@gmail.com' }
-        ]
-      }
-    }
+    raw_auth_response = JSON.parse(File.read('test/fixtures/omniauth_response.json'))
 
     auth = OmniauthResponse.new(response: raw_auth_response)
     user = User.from_omniauth(auth)
@@ -60,4 +34,3 @@ class UserTest < ActiveSupport::TestCase
     assert_equal auth.github_token, user.github_token
   end
 end
-# rubocop:enable Metrics/BlockLength
