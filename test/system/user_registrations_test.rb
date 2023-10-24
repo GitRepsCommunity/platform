9488e789da74105688c0766514c8e4352f09e071
Andrew J <jumanca@uw.edu>
2023-10-23 21:50:11 -0700

    Fixed linting

--- test/system/user_registrations_test.rb
+++ test/system/user_registrations_test.rb
@@ -3,5 +3,7 @@
 require 'application_system_test_case'
 
 class UserRegistrationsTest < ApplicationSystemTestCase
-  # TODO: Add tests for GitHub Authentication to replace email/password login
+  visit new_user_registration_url
+  click_on 'Sign up with GitHub'
+  assert_redirected_to %r{\Ahttps://github.com/login}
 end
