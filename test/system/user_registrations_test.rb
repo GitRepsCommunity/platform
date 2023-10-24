e1ba7763b4af80ab25c9b2eeeefa9cb20a566281
Andrew J <jumanca@uw.edu>
2023-10-24 14:32:23 -0700

    Updated user registration test to correctly check if redirection to GitHub auth page happens

--- test/system/user_registrations_test.rb
+++ test/system/user_registrations_test.rb
@@ -3,7 +3,9 @@
 require 'application_system_test_case'
 
 class UserRegistrationsTest < ApplicationSystemTestCase
-  visit new_user_registration_url
-  click_on 'Sign up with GitHub'
-  assert_redirected_to %r{\Ahttps://github.com/login}
+  test 'user registration with GitHub' do
+    visit '/users/sign_up'
+    click_on 'Sign up with GitHub'
+    assert(page.current_url.start_with?('https://github.com/login'), "Current URL is not as expected: #{current_url}")
+  end
 end
