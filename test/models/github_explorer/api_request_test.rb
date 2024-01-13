f9a498479281342382007c0cfd77ae9211d04deb
Tim Pile <tim@gitreps.com>
2024-01-13 15:24:43 -0800

    Fix Issue 87: Error when parsing array response

--- test/models/github_explorer/api_request_test.rb
+++ test/models/github_explorer/api_request_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'minitest/mock'
 
 module GithubExplorer
   class ApiRequestTest < ActiveSupport::TestCase
@@ -9,6 +10,28 @@ module GithubExplorer
 
       assert_nothing_raised do
         api_request.user
+      end
+    end
+
+    test 'send_request_and_save! handles json arrays' do
+      api_request = users(:one).api_requests.create
+
+      test_response = JSON.parse('[]')
+      api_request.stub :send_request, test_response do
+        api_request.send_request_and_save!
+        assert_equal test_response, api_request.response
+        assert api_request.response.is_a?(Array)
+      end
+    end
+
+    test 'send_request_and_save! handles json objects' do
+      api_request = users(:one).api_requests.create
+
+      test_response = JSON.parse('{}')
+      api_request.stub :send_request, test_response do
+        api_request.send_request_and_save!
+        assert_equal test_response, api_request.response
+        assert api_request.response.is_a?(Hash)
       end
     end
   end
